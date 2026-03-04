42303829facd70b21246507f811475ae5b2d8156
tmhabte <78515300+tmhabte@users.noreply.github.com>
2026-03-04 15:55:10 -0800

    Update OABS_util.py

--- HS-2025-bindcopoly-code/vertex_calc/OABS_util.py
+++ HS-2025-bindcopoly-code/vertex_calc/OABS_util.py
@@ -4,6 +4,55 @@ import scipy as sp
 from itertools import product
 
 class Polymer_soln:
+    """
+    Container object representing a polymer system used for RPA free-energy
+    calculations with reversible binding.
+
+    This class stores all thermodynamic and compositional parameters
+    required to compute binding states of marked polymers and evaluate
+    the RPA free energy over a range of chemical
+    potentials. A single instance defines a set of solutions spanning
+    (mu1, mu2), sufficient to generate one phase diagram.
+
+    Parameters
+    ----------
+    n_bind : int
+        Number of distinct binding states per marked site. (for AB system, n_bind = 2)
+    v_int : array-like
+        Interaction parameter matrix (n_bind x n_bind) governing nearest-neighbor bound guest interactions. 
+        Diagonal elements indicate self-interactions, non-diagonal elements indicate cross-interactions
+    e_m : array-like
+        Binding affinity associated with mathcing guest-host interactions
+    phi_p : float
+        Polymer volume fraction.
+    poly_marks : array-like
+        Sequence-level description of host (or marked) sites along the polymer backbone.
+        Shape (n_bind, M), where M is the number of avreaged monomers.
+    mu1_arr : array-like
+        Array of chemical potentials for species 1.
+    mu2_arr : array-like
+        Array of chemical potentials for species 2.
+    v_s : float
+        Solvent molecular volume.
+    v_m : float
+        Molecular volume of averaged monomer.
+    N : int
+        Degree of polymerization (total Kuhn segments per chain).
+    b : float
+        Kuhn length.
+
+    Attributes
+    ----------
+    phi_s : float
+        Solvent volume fraction (1 - phi_p).
+    alpha : float
+        Dimensionless prefactor to solvent strucutre factor 
+        under the incompressibility constraint.
+    M : int
+        Number of averaged monomers
+    N_m : float
+        Number of Kuhn segments per averaged monomer (N / M).
+    """
     def __init__(self, n_bind, v_int, e_m, phi_p, poly_marks, mu1_arr, mu2_arr, v_s, v_m, N, b):
         self.n_bind = n_bind
         self.v_int = v_int
@@ -20,3 +69,4 @@ class Polymer_soln:
         self.M = len(poly_marks[0])
         self.N_m = N / self.M
         self.b = b
+
